074d16d38ff9dd4bff9372a79dea06ef9a3f1cff
git2019 <aj1208370852@163.com>
2020-09-06 22:45:24 +0800

    增加注释

--- springboot2/src/main/java/com/chezj/demo/util/ToJson.java
+++ springboot2/src/main/java/com/chezj/demo/util/ToJson.java
@@ -2,9 +2,17 @@ package com.chezj.demo.util;
 
 import com.chezj.demo.entity.User;
 import net.sf.json.JSONObject;
-
+/**
+ * @Method 工具类
+ * @Author Administrator
+ * @Version  1.0
+ * @Description
+ * @param null
+ * @Return 
+ * @Exception 
+ * @Date 2020/9/6 
+ */
 public class ToJson {
-
     public static String ShowDetailInfo(User user) {
         JSONObject jsonpObject= JSONObject.fromObject(user);
         String str = jsonpObject.toString();
