b62db9a02ac89d0f21d382a84b2187d725da7596
chezj <chezj@bsfit.com.cn>
2020-09-18 17:23:00 +0800

    完成定时任务 完成切面日志 读取配置文件

--- springboot2/src/main/java/com/chezj/demo/controller/UserFindController.java
+++ springboot2/src/main/java/com/chezj/demo/controller/UserFindController.java
@@ -1,6 +1,10 @@
 package com.chezj.demo.controller;
 
+import cn.hutool.core.lang.Dict;
 import com.chezj.demo.dao.UserMapper;
+import com.chezj.demo.property.ApplicationProperty;
+import com.chezj.demo.property.DeveloperProperty;
+import com.chezj.demo.property.TestProperty;
 import com.chezj.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserFindController {
     private final UserMapper userMapper;
     private final UserService userService;
+    private final ApplicationProperty applicationProperty;
+    private final DeveloperProperty developerProperty;
+    private final TestProperty testProperty;
 
     @GetMapping("getUser")
     public String find(int id) {
@@ -24,5 +31,10 @@ public class UserFindController {
     @GetMapping("getUserDetail")
     public String ShowDetailInfo(int id) {
         return userService.ShowDetailUserInfo(userMapper.selectByPrimaryKey(id));
+    }
+
+    @GetMapping("/property")
+    public Dict index() {
+        return Dict.create().set("applicationProperty", applicationProperty).set("developerProperty", developerProperty).set("testProperty",testProperty);
     }
 }
